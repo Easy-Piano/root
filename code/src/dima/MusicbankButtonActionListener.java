6eeba43c14314a054628d8437b1397c0123c8e6e
Dmitry Grinko <grinko-d@mail.ru>
2014-04-24 18:37:42 +0400

    Листнеры для кнопок

--- code/src/dima/MusicbankButtonActionListener.java
+++ code/src/dima/MusicbankButtonActionListener.java
@@ -6,9 +6,9 @@ import java.awt.event.ActionListener;
 /**
  * Created by Дмитрий on 24.04.2014.
  */
-public class ExerciseButtonActionListener implements ActionListener {
+public class MusicbankButtonActionListener implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
-        //здесь происходит открытие окна с упражнением
+        //здесь происходит открытие окна с банком мелодий
     }
 }
