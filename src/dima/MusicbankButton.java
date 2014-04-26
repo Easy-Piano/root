a4d3acd5509e97c8ee7a18ba9ddb1dfa07216a6c
Dmitry Grinko <grinko-d@mail.ru>
2014-04-26 17:13:40 +0400

    Main buttons interface

--- src/dima/MusicbankButton.java
+++ src/dima/MusicbankButton.java
@@ -1,12 +1,13 @@
 package dima;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
  * Created by Дмитрий on 24.04.2014.
  */
-public class MusicbankButtonActionListener implements ActionListener {
+public class MusicbankButton extends JButton implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         //здесь происходит открытие окна с банком мелодий
