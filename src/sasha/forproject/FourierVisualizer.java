49c8c23024022083e74c9403c6a42718eead2083
EroshenkoA <furysham@mail.ru>
2014-04-27 15:44:57 +0400

    FourierVisualiser, amplitude only

--- src/sasha/forproject/FourierVisualizer.java
+++ src/sasha/forproject/FourierVisualizer.java
@@ -1,10 +1,32 @@
 package sasha.forproject;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import javax.swing.*;
+
 /**
  * Created by 1 on 26.04.2014.
  */
 public class FourierVisualizer implements Visualizer {
-    public void visualize(double[] array,long n){
-
+    private final static int N = 512;
+    public void visualize(double[] array,long sampleRate){//amplitude, not phase
+        XYSeries seriesmag = new XYSeries("Spectrum magnitude");
+        double minfrequency=sampleRate/N;
+        for (int j=0; j<N/2;j++){
+            seriesmag.add(minfrequency*j, Math.abs(array[j]));
+        }
+        XYDataset xyDataset = new XYSeriesCollection(seriesmag);
+        JFreeChart chart = ChartFactory.createXYLineChart("y=signal spectrum", "frequency,Hz", "magnitude",
+                xyDataset, PlotOrientation.VERTICAL, true, true, true);
+        JFrame frame = new JFrame("spectrum try");
+        frame.getContentPane().add(new ChartPanel(chart));
+        frame.setSize(800,600);
+        frame.setVisible(true);
     }
 }
--- src/sasha/forproject/SashaMain.java
+++ src/sasha/forproject/SashaMain.java
@@ -14,7 +14,7 @@ import javax.swing.*;
  * Created by 1 on 26.04.2014.
  */
 public class SashaMain {//shows spectrum finally
-    private final static int degree=256;
+    private static final int degree=512;//connected with FourierVisualizer.N (N=degree)
     private static int offset=100000;//to skip the beginning
     public static void main(String[] args){
         //String s = SashaMain.class.getClassLoader().getResource("Chopin.wav").getPath();
@@ -22,32 +22,10 @@ public class SashaMain {//shows spectrum finally
         //String s="C:\\Users\\Дмитрий\\Documents\\GitHub\\root\\src\\sasha\\forlearning\\Chopin.wav";
         WavWrapper WW=new WavWrapper(s);
         double[] v=WW.getArray(0,offset,degree);
-        double[] vv=new double[degree];
-        FFT.complexToComplex(1,degree,v,vv);
+        double[] vv=MyFFT.RealToReal(v);
+        //FFT.complexToComplex(1,degree,v,vv);//should i use 1 or -1??
         long sampleRate=WW.getSampleRate();
-        XYSeries seriesmag = new XYSeries("Spectrum magnitude");
-        double minfrequency=sampleRate/degree;
-        for (int j=0; j<degree/2;j++){
-            seriesmag.add(minfrequency*j, Math.abs(v[j]));
-        }
-        XYDataset xyDataset = new XYSeriesCollection(seriesmag);
-        JFreeChart chart = ChartFactory.createXYLineChart("y=spectrum(j)", "frequency", "magnitude",
-        xyDataset, PlotOrientation.VERTICAL, true, true, true);
-        JFrame frame = new JFrame("spectrum try");
-        frame.getContentPane().add(new ChartPanel(chart));
-        frame.setSize(800,600);
-        frame.setVisible(true);
-
-        XYSeries seriesphase = new XYSeries("Spectrum phase");
-        for (int j=0; j<degree/2;j++){
-            seriesphase.add(j*minfrequency, vv[j]);
-        }
-        XYDataset xyDatasetphase = new XYSeriesCollection(seriesphase);
-        JFreeChart chartphase = ChartFactory.createXYLineChart("y=spectrum(j)", "frequency", "phase",
-                xyDatasetphase, PlotOrientation.VERTICAL, true, true, true);
-        JFrame framephase = new JFrame("spectrum try");
-        framephase.getContentPane().add(new ChartPanel(chartphase));
-        framephase.setSize(800,600);
-        framephase.setVisible(true);
+        FourierVisualizer FV = new FourierVisualizer();
+        FV.visualize(vv,sampleRate);
     }
 }
