6678b0723b6d481eb22761ce8af2153a943d11df
restvik <restvik@gmail.com>
2014-04-30 18:07:30 +0400

    musician

--- src/Ivan/Music/Player.java
+++ src/Ivan/Music/Player.java
@@ -7,7 +7,7 @@ import javax.sound.midi.MidiChannel;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Synthesizer;
-
+// test ///
 public class Player {
 
     private MidiChannel[] channels = null;
